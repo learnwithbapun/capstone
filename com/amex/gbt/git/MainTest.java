e058ce22baa7c3c40ba46cdaf82948ba1a7ed4d4
Bapun <learnwithbapun@gmail.com>
2024-07-30 23:50:56 +0530

    Added

---
+++ com/amex/gbt/git/MainTest.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        
+        List<String> fruits = Arrays.asList("apple", "pineapple", "banana", "apple", "banana", "banana", "apple");
+        for (String string : fruits) {
+            System.out.println(string);
+            
+        }
+
+
+    }
+    
+}
